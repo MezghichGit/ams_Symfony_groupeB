70ff43bb2c83ea99f18f6aa25a6981723f8f3878
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-06-04 16:30:26 +0100

    ams list + add provider

---
+++ src/Controller/ProviderController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Provider;
+use App\Form\ProviderType;
+use App\Repository\ProviderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/provider")
+ */
+class ProviderController extends AbstractController
+{
+    /**
+     * @Route("/list", name="provider_list", methods={"GET"})
+     */
+    public function index(ProviderRepository $providerRepository): Response
+    {
+        $ecole="sesame";
+        $providers = $providerRepository->findAll();
+        return $this->render('provider/list.html.twig',
+        ['providers'=>$providers, 'ecole'=>$ecole]
+);
+
+        /*
+        return $this->render('provider/index.html.twig', [
+            'providers' => ,
+        ]);*/
+       // return new Response("<h1>Hello Symfony</h1>");
+    }
+
+
+    /**
+     * @Route("/ajout", name="provider_ajout", methods={"GET","POST"})
+     */
+    public function ajout(Request $request,ProviderRepository $providerRepository): Response
+    {
+      if($request->getMethod()=="POST")
+      {
+          $name = $request->get('name');
+          $email = $request->get('email');
+          $adress = $request->get('adress');
+          $provider = new Provider();
+
+          $provider->setName($name);
+          $provider->setEmail($email);
+          $provider->setAdress($adress);
+
+          $entityManager = $this->getDoctrine()->getManager();
+          $entityManager->persist($provider);
+          $entityManager->flush();
+
+          return $this->redirectToRoute('provider_list');
+    
+
+          //return new Response("Insertion : ".$name);
+      }  
+        
+        
+        return $this->render('provider/ajout.html.twig');
+
+    }
+
+    /**
+     * @Route("/new", name="provider_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $provider = new Provider();
+        $form = $this->createForm(ProviderType::class, $provider);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($provider);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('provider_index');
+        }
+
+        return $this->render('provider/new.html.twig', [
+            'provider' => $provider,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="provider_show", methods={"GET"})
+     */
+    public function show(Provider $provider): Response
+    {
+        return $this->render('provider/show.html.twig', [
+            'provider' => $provider,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="provider_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Provider $provider): Response
+    {
+        $form = $this->createForm(ProviderType::class, $provider);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('provider_index');
+        }
+
+        return $this->render('provider/edit.html.twig', [
+            'provider' => $provider,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="provider_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Provider $provider): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$provider->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($provider);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('provider_index');
+    }
+}
