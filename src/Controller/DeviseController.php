0543eadac1fb003fb08111ed63e98eb3ce054bbb
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-07-03 14:36:44 +0100

    ajax exemple sur les boutons

--- src/Controller/DeviseController.php
+++ src/Controller/DeviseController.php
@@ -35,6 +35,43 @@ class DeviseController extends AbstractController
 }
 
 
+/**
+* @Route("/ajax", name="ajax")
+*/
+public function ajax(Request $request)
+{
+    /* on récupère la valeur envoyée par la vue */
+    $personnage = $request->request->get('personnage');
+    
+    switch ($personnage) {
+            case 'Homer':
+                $titre = 'Simpsons';
+                $producteur = 'Matt Groening';
+            break;
+            case 'Cartman':
+                $titre = 'South Park';
+                $producteur = 'Trey Parker and Matt Stone';
+            break;
+            case 'Elsa':
+                $titre = 'Reine des neiges';
+                $producteur = 'Walt Disney Animation Studios';
+            break;
+            case 'Lord Farquaad':
+                $titre = 'Shrek';
+                $producteur = 'Dreamworks';
+    }
+    /* la réponse doit être encodée en JSON ou XML, on choisira le JSON
+    * la doc de Symfony est bien faite si vous devez renvoyer un objet
+    *
+    */
+    $response = new Response(json_encode(array(
+    'titre' => $titre,
+    'producteur' => $producteur
+    )));
+    
+    $response->headers->set('Content-Type', 'application/json');
+    return $response;
+}
     
     
     
